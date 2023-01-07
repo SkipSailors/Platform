107719882501268d505d8771aeb49d42d010588c
Skip Sailors <ssailors@wpc-edi.com>
2023-01-07 12:24:46 -0800

    listing 12-15

---
+++ MessageOpions.cs
@@ -0,0 +1,10 @@
+﻿namespace Platform
+{
+    using System.Security;
+
+    public class MessageOptions
+    {
+        public string CityName { get; set; } = "New York";
+        public string CountryName { get; set; } = "USA";
+    }
+}
