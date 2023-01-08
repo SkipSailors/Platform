5fa19e8b5966f993f8e3b289c1d5221eaba3b8c1
Skip Sailors <ssailors@wpc-edi.com>
2023-01-08 10:37:41 -0800

    listing 13-8

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ WebApplication app = builder.Build();
 
 app.MapGet("{first}/{second}/{third}", async (context) =>
 {
-    await context.Response.WriteAsync("Request was routed");
+    await context.Response.WriteAsync("Request was routed\n");
     foreach (KeyValuePair<string, object?> kvp in context.Request.RouteValues)
     {
         await context.Response.WriteAsync($"{kvp.Key}:{kvp.Value}\n");
