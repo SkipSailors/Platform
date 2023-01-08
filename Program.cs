69480d7aef9dc3100c6bc95160086d427102ea8c
Skip Sailors <ssailors@wpc-edi.com>
2023-01-07 16:18:09 -0800

    lisstingg 13-3

--- Program.cs
+++ Program.cs
@@ -1,51 +1,13 @@
 using Platform;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-builder.Services.Configure<MessageOptions>(options =>
-{
-    options.CityName = "Albany";
-});
-
 WebApplication app = builder.Build();
 
-app.Map("/branch", branch =>
+app.UseMiddleware<Population>();
+app.UseMiddleware<Capital>();
+app.Run(async (context) =>
 {
-    branch.Run(new QueryStringMiddleware().Invoke);
+    await context.Response.WriteAsync("Terminal Middleware Reached");
 });
-
-app.Use(async (context, next) =>
-{
-    await next();
-    await context.Response.WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
-});
-
-app.Use(async (context, next) =>
-{
-    if (context.Request.Path == "/short")
-    {
-        await context.Response.WriteAsync("Request Short Circuited");
-    }
-    else
-    {
-        await next();
-    }
-});
-
-app.Use(async (context, next) =>
-{
-    if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
-    {
-        context.Response.ContentType = "text/plain";
-        await context.Response.WriteAsync("Custom Middleware \n");
-    }
-
-    await next();
-});
-
-app.UseMiddleware<QueryStringMiddleware>();
-app.UseMiddleware<LocationMiddleware>();
-
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
