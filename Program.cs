51867e5a90a942a17190787bc7ee2b555467e4a2
Skip Sailors <ssailors@wpc-edi.com>
2023-01-08 22:35:47 -0800

    lissting 13-28

--- Program.cs
+++ Program.cs
@@ -8,11 +8,27 @@ builder.Services.Configure<RouteOptions>(opts =>
 
 WebApplication app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    Endpoint? end = context.GetEndpoint();
+    if (end != null)
+    {
+        await context.Response.WriteAsync($"{end.DisplayName} selected\n");
+    }
+    else
+    {
+        await context.Response.WriteAsync("no endpoint selected\n");
+    }
+
+    await next();
+});
+
 app
     .Map("{number:int}", async context =>
     {
         await context.Response.WriteAsync("Routed to the int endpoint\n");
     })
+    .WithDisplayName("Int Endpoint")
     .Add(b => ((RouteEndpointBuilder)b).Order = 1);
 app
     .Map("{number:double}",
@@ -20,6 +36,7 @@ app
         {
             await context.Response.WriteAsync("Routed to the double endpoint\n");
         })
+    .WithDisplayName("Double Endpoint")
     .Add(b => ((RouteEndpointBuilder)b).Order = 2);
 
 app.MapFallback(async context =>
