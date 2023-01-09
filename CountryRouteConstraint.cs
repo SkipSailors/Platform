216b0e5b964b770ac4daeb4a13b727e810e28a66
Skip Sailors <ssailors@wpc-edi.com>
2023-01-08 21:52:57 -0800

    listing 13-24

---
+++ CountryRouteConstraint.cs
@@ -0,0 +1,17 @@
+﻿namespace Platform
+{
+    public class CountryRouteConstraint: IRouteConstraint
+    {
+        private static readonly string[] Countries = { "Uk", "france", "monaco" };
+        public bool Match(
+            HttpContext? httpContext,
+            IRouter? route,
+            string routeKey,
+            RouteValueDictionary values,
+            RouteDirection routeDirection)
+        {
+            string segmentValue = values[routeKey] as string ?? string.Empty;
+            return Array.IndexOf(Countries, segmentValue.ToLower()) > -1;
+        }
+    }
+}
