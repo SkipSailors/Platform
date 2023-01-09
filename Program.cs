1c5a9cf4bbc5f9fa8c68639709eb52a9e0136005
Skip Sailors <ssailors@wpc-edi.com>
2023-01-08 21:39:55 -0800

    lstin 13-23

--- Program.cs
+++ Program.cs
@@ -15,5 +15,9 @@ app.MapGet("capital/{country:regex(^uk|france|monaco$)}", Capital.Endpoint);
 app
     .MapGet("size/{city?}", Population.Endpoint)
     .WithMetadata(new RouteNameMetadata("population"));
+app.MapFallback(async context =>
+{
+    await context.Response.WriteAsync("Routed to fallback endpoint\n");
+});
 
 app.Run();
