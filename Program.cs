fa98f13baed1afd8db512904db3564a4d377611c
Skip Sailors <ssailors@wpc-edi.com>
2023-01-08 22:05:27 -0800

    listiing 13-25

--- Program.cs
+++ Program.cs
@@ -1,16 +1,14 @@
 using Platform;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<RouteOptions>(opts =>
+{
+    opts.ConstraintMap.Add("countryName", typeof(CountryRouteConstraint));
+});
+
 WebApplication app = builder.Build();
 
-app.MapGet("{first:alpha:length(3)}/{second:bool}", async (context) =>
-{
-    await context.Response.WriteAsync("Request was routed\n");
-    foreach (KeyValuePair<string, object?> kvp in context.Request.RouteValues)
-    {
-        await context.Response.WriteAsync($"{kvp.Key}:{kvp.Value}\n");
-    }
-});
+app.MapGet("capital/{country:countryName}", Capital.Endpoint);
 app.MapGet("capital/{country:regex(^uk|france|monaco$)}", Capital.Endpoint);
 app
     .MapGet("size/{city?}", Population.Endpoint)
