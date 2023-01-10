2d331fe91f5f51e26a9833c6e729533c8d9b0ec1
Skip Sailors <ssailors@wpc-edi.com>
2023-01-09 23:37:12 -0800

    listing 14-4

---
+++ WeatherEndpoint.cs
@@ -0,0 +1,9 @@
+﻿namespace Platform;
+
+public static class WeatherEndpoint
+{
+    public static async Task Endpoint(HttpContext context)
+    {
+        await context.Response.WriteAsync("Middleware Class: It is cloudy in Milan.");
+    }
+}
