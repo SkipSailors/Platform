a2ab2469c5d8b0f3b4d0b6b935829974dba77eff
Skip Sailors <ssailors@wpc-edi.com>
2023-01-10 22:52:35 -0800

    listing 14-9

---
+++ Services/TypeBroker.cs
@@ -0,0 +1,8 @@
+﻿namespace Platform.Services
+{
+    public static class TypeBroker
+    {
+        private static readonly IResponseFormatter formatter = new TextResponseFormatter();
+        public static IResponseFormatter Formatter => formatter;
+    }
+}
