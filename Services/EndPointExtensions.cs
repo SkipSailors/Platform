bebeadcf5f69b82168b0c76ff319aa7455159be7
Skip Sailors <ssailors@wpc-edi.com>
2023-01-10 23:44:18 -0800

    listing 14-17

---
+++ Services/EndPointExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Platform.Services
+{
+    public static class EndPointExtensions
+    {
+        public static void MapWeather(this IEndpointRouteBuilder app, string path)
+        {
+            IResponseFormatter formatter = app
+                .ServiceProvider
+                .GetRequiredService<IResponseFormatter>();
+            app.MapGet(
+                path,
+                context => Platform.WeatherEndpoint.Endpoint(context, formatter));
+        }
+    }
+}
