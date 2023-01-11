7dd6dffc97ab05f0ddbd994b718db705e33b4e62
Skip Sailors <ssailors@wpc-edi.com>
2023-01-10 22:46:53 -0800

    listing 14-8

--- Program.cs
+++ Program.cs
@@ -4,15 +4,14 @@ using Platform.Services;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 WebApplication app = builder.Build();
 app.UseMiddleware<WeatherMiddleware>();
-IResponseFormatter formatter = new TextResponseFormatter();
 app.MapGet("middleware/function", async (context) =>
 {
-    await formatter.Format(context, "Middleare Function: It is snowing in Chicago");
+    await TextResponseFormatter.Singleton.Format(context, "Middleare Function: It is snowing in Chicago");
 });
 app.MapGet("middleware/class", WeatherEndpoint.Endpoint);
 app.MapGet("endpoint/function", async (context) =>
 {
-    await formatter.Format(context, "Endpoint Function: It is sunny in LA");
+    await TextResponseFormatter.Singleton.Format(context, "Endpoint Function: It is sunny in LA");
 });
 
 app.Run();
