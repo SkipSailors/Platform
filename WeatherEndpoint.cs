c55477e739bd894e27152924f60023a808643d62
Skip Sailors <ssailors@wpc-edi.com>
2023-01-10 23:28:55 -0800

    lissting 14-15

--- WeatherEndpoint.cs
+++ WeatherEndpoint.cs
@@ -1,9 +1,12 @@
 ﻿namespace Platform;
 
+using Services;
+
 public static class WeatherEndpoint
 {
     public static async Task Endpoint(HttpContext context)
     {
-        await context.Response.WriteAsync("Middleware Class: It is cloudy in Milan.");
+        IResponseFormatter formatter = context.RequestServices.GetRequiredService<IResponseFormatter>();
+        await formatter.Format(context, "Middleware Class: It is cloudy in Milan.");
     }
 }
