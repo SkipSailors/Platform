73bdd1b12fef9703c0b3597a8fe8c72cffcf420e
Skip Sailors <ssailors@wpc-edi.com>
2023-01-13 08:21:52 -0800

    listing 14-30

--- Services/EndPointExtensions.cs
+++ Services/EndPointExtensions.cs
@@ -15,15 +15,17 @@ public static class EndPointExtensions
             throw new Exception("Method cannot be used");
         }
 
-        T endpointInstance = ActivatorUtilities.CreateInstance<T>(app.ServiceProvider);
         ParameterInfo[] methodParams = methodInfo!.GetParameters();
         app.MapGet(
             path,
             context =>
-                (Task)methodInfo.Invoke(
-                    endpointInstance,
+            {
+                T endpointInstance = ActivatorUtilities.CreateInstance<T>(context.RequestServices);
+                return (Task)methodInfo.Invoke(
+                    endpointInstance!,
                     methodParams.Select(p => p.ParameterType == typeof(HttpContext)
                         ? context
-                        : context.RequestServices.GetService(p.ParameterType)).ToArray())!);
+                        : context.RequestServices.GetService(p.ParameterType)).ToArray())!;
+            });
     }
 }
