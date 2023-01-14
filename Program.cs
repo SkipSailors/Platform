5f916a1b011f0edcbb8a648b229d5caeb729dc70
Skip Sailors <ssailors@wpc-edi.com>
2023-01-14 10:51:35 -0800

    listing 15-4

--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 WebApplication app = builder.Build();
 
+app.MapGet("config", async (HttpContext context, IConfiguration configuration) =>
+{
+    string defaultDebug = configuration["Logging:LogLevel:Default"];
+    await context.Response.WriteAsync($"The config setting is {defaultDebug}");
+});
+
 app.MapGet("/", async context =>
 {
     await context.Response.WriteAsync("Hello World");
