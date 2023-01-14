bc9ddef0529cbd10e3ab6830ff49ce2b63b0171b
Skip Sailors <ssailors@wpc-edi.com>
2023-01-14 13:04:28 -0800

    liting 15-12

--- Program.cs
+++ Program.cs
@@ -3,17 +3,19 @@ using Platform;
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 ConfigurationManager servicesConfig = builder.Configuration;
 builder.Services.Configure<MessageOptions>(servicesConfig.GetSection("Location"));
+IWebHostEnvironment servicesEnvironment = builder.Environment;
 
 WebApplication app = builder.Build();
 IConfiguration pipelineConfig = app.Configuration;
+IWebHostEnvironment pipelineEnv = app.Environment;
 
 app.UseMiddleware<LocationMiddleware>();
-app.MapGet("config", async (HttpContext context, IConfiguration configuration) =>
+app.MapGet("config", async (HttpContext context, IConfiguration configuration, IWebHostEnvironment env) =>
 {
     string defaultDebug = configuration["Logging:LogLevel:Default"];
     await context.Response.WriteAsync($"The config setting is {defaultDebug}\n");
     string environ = configuration["ASPNETCORE_ENVIRONMENT"];
-    await context.Response.WriteAsync($"The env setting is {environ}");
+    await context.Response.WriteAsync($"\nThe env setting is {env.EnvironmentName}");
 });
 
 app.MapGet("/", async context =>
