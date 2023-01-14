66761cd1ab275c04d0c49a67c52ac8703639af44
Skip Sailors <ssailors@wpc-edi.com>
2023-01-14 11:33:54 -0800

    listing 15-8

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using Platform;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+ConfigurationManager servicesConfig = builder.Configuration;
+builder.Services.Configure<MessageOptions>(servicesConfig.GetSection("Location"));
 
 WebApplication app = builder.Build();
+IConfiguration pipelineConfig = app.Configuration;
 
+app.UseMiddleware<LocationMiddleware>();
 app.MapGet("config", async (HttpContext context, IConfiguration configuration) =>
 {
     string defaultDebug = configuration["Logging:LogLevel:Default"];
