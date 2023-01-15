50c4f61dde3963030a822cb453686b9423f8b021
Skip Sailors <ssailors@wpc-edi.com>
2023-01-14 18:05:59 -0800

    listing 15-19

--- Program.cs
+++ Program.cs
@@ -1,30 +1,9 @@
 using Platform;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-ConfigurationManager servicesConfig = builder.Configuration;
-builder.Services.Configure<MessageOptions>(servicesConfig.GetSection("Location"));
-IWebHostEnvironment servicesEnvironment = builder.Environment;
 
 WebApplication app = builder.Build();
-IConfiguration pipelineConfig = app.Configuration;
-IWebHostEnvironment pipelineEnv = app.Environment;
 
-app.UseMiddleware<LocationMiddleware>();
-app.MapGet("config", async (HttpContext context, IConfiguration configuration, IWebHostEnvironment env) =>
-{
-    string defaultDebug = configuration["Logging:LogLevel:Default"];
-    await context.Response.WriteAsync($"The config setting is {defaultDebug}\n");
-    string environ = configuration["ASPNETCORE_ENVIRONMENT"];
-    await context.Response.WriteAsync($"\nThe env setting is {env.EnvironmentName}");
-    string wsId = configuration["WebService:Id"];
-    string wsKey = configuration["WebService:Key"];
-    await context.Response.WriteAsync($"\nThe secret ID setting is {wsId}");
-    await context.Response.WriteAsync($"\nThe secret Key setting is {wsKey}");
-});
-
-app.MapGet("/", async context =>
-{
-    await context.Response.WriteAsync("Hello World");
-});
+app.MapGet("population/{city?}", Population.Endpoint);
 
 app.Run();
