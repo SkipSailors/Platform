4a3b104a1fda8f2115e6b2ce14ea9429f9b02ffa
Skip Sailors <ssailors@wpc-edi.com>
2023-01-14 21:27:59 -0800

    listing 15-28

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
+using Microsoft.AspNetCore.HttpLogging;
 using Platform;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpLogging(opts =>
+{
+    opts.LoggingFields = HttpLoggingFields.RequestMethod |
+                         HttpLoggingFields.RequestPath |
+                         HttpLoggingFields.ResponseStatusCode;
+});
 
 WebApplication app = builder.Build();
 
 app.UseHttpLogging();
 
-// ILogger logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
-
-// logger.LogDebug("pipeline configuration starting");
-
 app.MapGet("population/{city?}", Population.Endpoint);
-
-// logger.LogDebug("pipeline configuration complete");
 
 app.Run();
