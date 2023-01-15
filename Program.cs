e7053dbfc251f9f9d76462e927186681bcb1c510
Skip Sailors <ssailors@wpc-edi.com>
2023-01-14 18:20:45 -0800

    listing 15-21

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 WebApplication app = builder.Build();
 
+app.Logger.LogDebug("pipeline configuration starting");
+
 app.MapGet("population/{city?}", Population.Endpoint);
+
+app.Logger.LogDebug("pipeline configuration complete");
 
 app.Run();
