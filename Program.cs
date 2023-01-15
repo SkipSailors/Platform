c89b27b0c84d12571a4b31dad0d59e3103b35692
Skip Sailors <ssailors@wpc-edi.com>
2023-01-14 22:40:59 -0800

    lsting 16-1

--- Program.cs
+++ Program.cs
@@ -1,25 +1,6 @@
-using Microsoft.AspNetCore.HttpLogging;
-using Microsoft.Extensions.FileProviders;
-using Platform;
-
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHttpLogging(opts =>
-{
-    opts.LoggingFields = HttpLoggingFields.RequestMethod |
-                         HttpLoggingFields.RequestPath |
-                         HttpLoggingFields.ResponseStatusCode;
-});
-
 WebApplication app = builder.Build();
 
-app.UseHttpLogging();
-IWebHostEnvironment env = app.Environment;
-app.UseStaticFiles(new StaticFileOptions
-{
-    FileProvider = new PhysicalFileProvider($"{env.ContentRootPath}/staticFiles"),
-    RequestPath = "/files"
-});
-
-app.MapGet("population/{city?}", Population.Endpoint);
+app.MapFallback(async context => await context.Response.WriteAsync("Hello World"));
 
 app.Run();
