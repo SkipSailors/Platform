092998bfb18437549dda318d7a3c6969c1b22797
Skip Sailors <ssailors@wpc-edi.com>
2023-01-14 18:53:59 -0800

    listing 15-26

--- Program.cs
+++ Program.cs
@@ -4,12 +4,14 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 WebApplication app = builder.Build();
 
-ILogger logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+app.UseHttpLogging();
 
-logger.LogDebug("pipeline configuration starting");
+// ILogger logger = app.Services.GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+
+// logger.LogDebug("pipeline configuration starting");
 
 app.MapGet("population/{city?}", Population.Endpoint);
 
-logger.LogDebug("pipeline configuration complete");
+// logger.LogDebug("pipeline configuration complete");
 
 app.Run();
