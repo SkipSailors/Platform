73793fadbe4fb46401469e88c99b95585633eee1
Skip Sailors <ssailors@wpc-edi.com>
2023-01-20 22:15:14 -0800

    listing 16-13

--- Program.cs
+++ Program.cs
@@ -7,7 +7,18 @@ builder.Services.AddSession(opts =>
     opts.IdleTimeout = TimeSpan.FromMinutes(30);
     opts.Cookie.IsEssential = true;
 });
+builder.Services.AddHsts(opts =>
+{
+    opts.MaxAge = TimeSpan.FromDays(1);
+    opts.IncludeSubDomains = true;
+});
 WebApplication app = builder.Build();
+
+if (app.Environment.IsProduction())
+{
+    app.UseHsts();
+}
+
 app.UseHttpsRedirection();
 app.UseSession();
 app.UseMiddleware<ConsentMiddleware>();
@@ -22,7 +33,7 @@ app.MapGet("/session", async context =>
 });
 app.MapFallback(async context =>
 {
-    await context.Response.WriteAsync($"{context.Request.IsHttps}\n");
+    await context.Response.WriteAsync($"TLS: {context.Request.IsHttps}\n");
     await context.Response.WriteAsync("Hello World");
 });
 app.Run();
