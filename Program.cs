d5ace887b7015e457392b54c2d1212d69ab9129c
Skip Sailors <ssailors@wpc-edi.com>
2023-01-20 22:06:55 -0800

    listing 16-12

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddSession(opts =>
     opts.Cookie.IsEssential = true;
 });
 WebApplication app = builder.Build();
+app.UseHttpsRedirection();
 app.UseSession();
 app.UseMiddleware<ConsentMiddleware>();
 app.MapGet("/session", async context =>
