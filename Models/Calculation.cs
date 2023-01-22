457cab080d9fae89b316a740dbb818f931452747
Skip Sailors <ssailors@wpc-edi.com>
2023-01-22 12:16:19 -0800

    listing 17-19

--- Models/Calculation.cs
+++ Models/Calculation.cs
@@ -1,6 +1,8 @@
-﻿namespace Platform.Models
+﻿namespace Platform.Models;
+
+public class Calculation
 {
-    public class Calculation
-    {
-    }
+    public long Id { get; set; }
+    public int Count { get; set; }
+    public long Result { get; set; }
 }
---
+++ Models/CalculationContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Platform.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+public class CalculationContext:DbContext
+{
+    public CalculationContext(DbContextOptions<CalculationContext> opts): base(opts){}
+    public DbSet<Calculation> Calculations => Set<Calculation>();
+}
