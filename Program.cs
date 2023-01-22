a4a8dc226b69c90c750f0ed60ea79436781ea9c1
Skip Sailors <ssailors@wpc-edi.com>
2023-01-21 19:37:53 -0800

    listing 17-6

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@ using Platform;
 using Platform.Services;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDistributedMemoryCache(opts =>
+{
+    opts.SizeLimit = 200;
+});
+
 WebApplication app = builder.Build();
 
 app.MapEndpoint<SumEndpoint>("/sum/{count:int=1000000000}");
